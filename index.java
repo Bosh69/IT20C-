bc7da469c895969c92962a77a7c7c2d0884ef5a1
Bosh69 <bvshermusic@gmail.com>
2024-09-24 15:23:32 +0800

    Arrays

---
+++ index.java
@@ -0,0 +1,10 @@
+package simplearray;
+
+public class SimpleArray {
+    public static void main(String[] args) {
+        
+        //Simple Implementation
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("First element in array: " + numbers[0]);
+    }
+}
